a3704c576c67e3a21d0c15682758dcc7a934f955
adriiianiiitaj <adriana.love.21@gmail.com>
2016-03-27 19:55:28 -0600

    cambiar el formato de fecha

--- js/scripts-admin.js
+++ js/scripts-admin.js
@@ -1,9 +1,32 @@
 $(function() {
-  $( "#fecha" ).datepicker();
+  $( "#fecha" ).datepicker({
+    dateFormat: "dd-mm-yy"
+  });
 
-  $( "#fecha_nacimiento" ).datepicker();
+  $( "#fecha_nacimiento" ).datepicker({
+    dateFormat: "dd-mm-yy"
+  });
 
-  $( "#desde" ).datepicker();
+  //$( "#desde" ).datepicker();
 
-  $( "#hasta" ).datepicker();
+  //$( "#hasta" ).datepicker();
+
+  $( "#desde" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      dateFormat: "dd-mm-yy",
+      onClose: function( selectedDate ) {
+        $( "#hasta" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#hasta" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      dateFormat: "dd-mm-yy",
+      onClose: function( selectedDate ) {
+        $( "#desde" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
 });
